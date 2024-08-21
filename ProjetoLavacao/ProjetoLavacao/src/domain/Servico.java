480fe286b568d4a3618b728acec719584b235cc2
Hátran <151634971+0hatran0@users.noreply.github.com>
2024-08-21 10:16:27 -0300

    Add files via upload

---
+++ ProjetoLavacao/ProjetoLavacao/src/domain/Servico.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author PC
+ */
+public class Servico {
+    private int id; 
+    private String descricao;
+    private double valor;
+    private static int pontos;
+
+    ECategoria categoria = ECategoria.PADRAO;
+    
+    public Servico() {
+    }
+
+    public Servico(int id, String descricao, double valor, ECategoria categoria) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    // Pontuação será static, entao apenas poderá ser retornada.
+    public int getPontos() {
+        return pontos;
+    }
+
+    public ECategoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(ECategoria categoria) {
+        this.categoria = categoria;
+    }
+}
